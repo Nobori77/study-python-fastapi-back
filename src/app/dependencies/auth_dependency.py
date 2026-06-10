08024328a4150d6e800a1e9a4f8335012e529e7e
Nobori77 <minginew77@gmail.com>
2026-06-10 09:23:48 +0900

    fix auth dependency

--- src/app/dependencies/auth_dependency.py
+++ src/app/dependencies/auth_dependency.py
@@ -1,18 +1,14 @@
 from fastapi import Request, HTTPException, status, Depends
-from app.schemas.member_schema import MemberClaimsDTO
 from app.schemas.auth_schema import JwtTokenDTO, AuthContextDTO
 from app.utils.jwt_token_util import parse_token
 from app.services.redis_service import RedisService, get_redis_service
 from jose import JWTError
 
-<<<<<<< HEAD
+
 def get_auth_context(
-        request: Request,
-        redis_service: RedisService = Depends(get_redis_service)
-        ) -> JwtTokenDTO:
-=======
-def get_auth_context(request: Request) -> JwtTokenDTO:
->>>>>>> cd449f19a4adfe998f21c733d1b95bc2ae7e2bfe
+    request: Request,
+    redis_service: RedisService = Depends(get_redis_service)
+) -> AuthContextDTO:
     access_token = request.cookies.get("access_token")
     refresh_token = request.cookies.get("refresh_token")
 
@@ -21,16 +17,16 @@ def get_auth_context(request: Request) -> JwtTokenDTO:
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="접근 권한 없음(access token)"
         )
-    
-    if not redis_service.is_blacklisted_token(access_token):
+
+    if redis_service.is_blacklisted_token(access_token):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="만료된 토큰 입니다."
         )
-    
-    try:
 
+    try:
         member_claims = parse_token(access_token)
+
         tokens = JwtTokenDTO(
             access_token=access_token,
             refresh_token=refresh_token
@@ -41,13 +37,8 @@ def get_auth_context(request: Request) -> JwtTokenDTO:
             tokens=tokens
         )
 
-
     except JWTError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="유효하지 않은 토큰"
         )
-    
-
-
-    
